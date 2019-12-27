ef3a51344cc4acc497c4bfed851e6484966b9ea7
Anna Selivanova <foxy.eyed@gmail.com>
2019-12-27 09:23:54 +0400

    Show book by id

--- app/serializers/api/v1/book_serializer.rb
+++ app/serializers/api/v1/book_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::BookSerializer < ActiveModel::Serializer
-  attributes :title, :subtitle, :authors, :publisher, :description, :cover_image, :page_count
+  attributes :id, :title, :subtitle, :authors, :publisher, :description, :cover_image, :page_count
 end
