36aafbf317e8c6f1b8b9fcccfc8daae3b7cd16c2
Anna Selivanova <foxy.eyed@gmail.com>
2019-12-27 14:08:29 +0400

    Add endpoint to receive auth data from client

--- app/services/user/auth_with_google_oauth.rb
+++ app/services/user/auth_with_google_oauth.rb
@@ -1,14 +1,11 @@
 class User::AuthWithGoogleOauth
   def call(auth)
-    user = User.find_by(google_uid: auth.uid.to_s)
-
-    unless user
-      user = User.new(email: auth.info[:email], google_uid: auth.uid, first_name: auth.info[:first_name],
-                      last_name: auth.info[:last_name], remote_avatar_url: auth.info[:image])
-    end
+    user = User.find_by(google_uid: auth[:google_uid])
+    user = User.new(auth) unless user
 
     return Result::Failure.new(user.errors) unless user.save
 
-    Result::Success.new(user)
+    token = JsonWebToken.encode(google_uid: user.google_uid)
+    Result::Success.new(token)
   end
 end
