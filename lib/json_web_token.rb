8417a6d0f7f7605a765c70db866e7058c97eea57
Anna Selivanova <foxy.eyed@gmail.com>
2019-12-27 15:26:02 +0400

    Fix jwt encoder

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,11 +2,11 @@ class JsonWebToken
   class << self
     def encode(payload, lifetime = 7.days)
       payload[:exp] = (Time.current + lifetime).to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, ENV["SECRET_KEY_BASE"])
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, ENV["SECRET_KEY_BASE"])[0]
       HashWithIndifferentAccess.new body
     rescue JWT::DecodeError
       nil
