7140848c6453121755f2aa48aa1674668823e233
Anna Selivanova <foxy.eyed@gmail.com>
2019-12-30 10:31:12 +0400

    Add book reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :book_reader_interactions
   has_many :books, through: :book_reader_interactions
 
+  has_many :reviews
+
   # Does not work on Heroku
   # https://github.com/carrierwaveuploader/carrierwave/wiki/How-to%3A-Make-Carrierwave-work-on-Heroku
   # mount_uploader :avatar, AvatarUploader
