7b4872735829310a0537ba83dd4d5101d1224ee1
mrkmx <maximov@appbooster.ru>
2019-12-30 16:00:50 +0400

    layout updated

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,20 +1,21 @@
 <template>
   <v-app>
     <v-container fluid>
+      <AddBook />
       <v-tabs-items v-model="tabs">
         <v-tab-item
           value="bookslist"
         >
           <v-row>
+            <Search />
+          </v-row>
+          <v-row>
             <v-col cols="12">
-              <v-row
-                align="start"
-                justify="center"
-                class="grey lighten-5"
-              >
-                <BooksList />
-              </v-row>
+              <h1 class="headline">Книги</h1>
             </v-col>
+          </v-row>
+          <v-row>
+            <BooksList />
           </v-row>
         </v-tab-item>
         <v-tab-item
@@ -26,19 +27,18 @@
       <v-tabs
         v-model="tabs"
         fixed-tabs
+        color="teal"
       >
         <v-tab
           href="#bookslist"
-          class="primary--text"
         >
-          <v-icon>library_books</v-icon>
+          <v-icon color="teal">library_books</v-icon>
         </v-tab>
 
         <v-tab
           href="#user-profile"
-          class="primary--text"
         >
-          <v-icon>account_circle</v-icon>
+          <v-icon color="teal">account_circle</v-icon>
         </v-tab>
         <v-tabs-slider></v-tabs-slider>
       </v-tabs>
@@ -48,11 +48,15 @@
 
 <script>
 import BooksList from 'components/bookslist'
+import Search from 'components/search'
+import AddBook from 'components/addbook'
 import UserProfile from 'components/userprofile'
 
 export default {
   components: {
     BooksList,
+    AddBook,
+    Search,
     UserProfile
   },
   data: function() {
@@ -67,5 +71,8 @@ export default {
 p {
   font-size: 2em;
   text-align: center;
+}
+.theme--light.v-tabs-items {
+  background: none;
 }
 </style>
--- app/javascript/plugins/vuetify.js
+++ app/javascript/plugins/vuetify.js
@@ -6,9 +6,7 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 Vue.use(Vuetify)
 
 const opts = {
-  theme: {
-    dark: true,
-  },
+
 }
 
 export default new Vuetify(opts)
