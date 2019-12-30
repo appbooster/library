7f28eb8671b11f737fbb665192b8be8b0378d40b
Alexander <maximov@appbooster.ru>
2019-12-30 16:25:26 +0400

    layout updated (#27)

---
+++ app/javascript/components/search.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-col cols="12">
+        <v-text-field
+                label="Книги"
+                append-icon="search"
+                rounded
+                clearable
+                placeholder="Поиск книги"
+                solo
+        ></v-text-field>
+    </v-col>
+</template>
